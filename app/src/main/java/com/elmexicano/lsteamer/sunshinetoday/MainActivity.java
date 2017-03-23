3bae1a656407d02cea9f0c2aa92d58b3c53db599
lsteamer <lsteamer@gmail.com>
2017-03-23 12:32:14 +0100

    03 - GPS Class created

--- app/src/main/java/com/elmexicano/lsteamer/sunshinetoday/MainActivity.java
+++ app/src/main/java/com/elmexicano/lsteamer/sunshinetoday/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationServices;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,15 +25,12 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FusedLocationProviderApi locationProviderApi = LocationServices.FusedLocationApi;
+
+
+
     //Inner class that retrieves the source code.
     public class DownloadTask extends AsyncTask<String, Void, String> {
-
-
-
-
-
-
-
 
         @Override
         protected String doInBackground(String... strings) {
@@ -49,19 +49,23 @@ public class MainActivity extends AppCompatActivity {
             int numDays = 7;
 
 
+
+
             try {
 
                 // Construct the URL for the OpenWeatherMap query
 
                 final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
-                final String QUERY_PARAM = "q";
+                final String LATITUDE_PARAM = "lat";
+                final String LONGITUDE_PARAM = "lot";
                 final String FORMAT_PARAM = "mode";
                 final String UNITS_PARAM = "units";
                 final String DAYS_PARAM = "cnt";
                 final String APPID_PARAM = "APPID";
 
                 Uri UriU = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, strings[0])
+                        .appendQueryParameter(LATITUDE_PARAM, strings[0])
+                        .appendQueryParameter(LONGITUDE_PARAM, strings[1])
                         .appendQueryParameter(FORMAT_PARAM, format)
                         .appendQueryParameter(UNITS_PARAM, units)
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
@@ -72,14 +76,19 @@ public class MainActivity extends AppCompatActivity {
                 URL url = new URL(UriU.toString());
 
 
+
                 //Accessing the URL
                 urlCon = (HttpURLConnection) url.openConnection();
                 urlCon.setRequestMethod("GET");
                 urlCon.connect();
 
+
+                Log.i("Day Forecast","Nope4");
+
                 // Read the input stream into a String
                 InputStream inputStream = urlCon.getInputStream();
                 StringBuffer buffer = new StringBuffer();
+
                 if (inputStream == null) {
                     // Nothing to do.
                     weatherJSONStr = null;
@@ -91,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
                     // Adding a newline as buffer for debugging.
                     buffer.append(line + "\n");
                 }
-
                 if (buffer.length() == 0) {
                     // Stream was empty.  No point in parsing.
                     return null;
@@ -139,35 +147,33 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 JSONObject weatherJSONObj = new JSONObject(result);
-                JSONArray weatherArray = weatherJSONObj.getJSONArray("list");
+                JSONArray weatherJSONArray = weatherJSONObj.getJSONArray("list");
 
 
-                for (int i = 0; i < weatherArray.length(); i++){
+                for (int i = 0; i < weatherJSONArray.length(); i++){
 
-                    String main;
+                    String mainTemperature;
                     String description;
                     String highAndLow;
 
                     // Get the JSON object representing the day
-                    JSONObject dayForecast = weatherArray.getJSONObject(i);
+                    JSONObject dayForecast = weatherJSONArray.getJSONObject(i);
 
 
 
                     // description is in a child array called "weather", which is 1 element long.
                     JSONObject weatherObject = dayForecast.getJSONArray("weather").getJSONObject(0);
-                    main = weatherObject.getString("main");
+                    mainTemperature = weatherObject.getString("main");
                     description = weatherObject.getString("description");
 
                     JSONObject temperatureObject = dayForecast.getJSONObject("temp");
+
                     double high = temperatureObject.getDouble("max");
                     double low = temperatureObject.getDouble("min");
 
                     highAndLow = formatHighLows(high, low);
 
-
-
-
-                    Log.i("Day Forecast",main+" - "+ highAndLow + " - "+description);
+                    Log.i("Day Forecast",mainTemperature+" - "+ highAndLow + " - "+description);
 
                 }
 
@@ -192,7 +198,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         //String that catches the result
         String  result="";
 
@@ -201,7 +206,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         try {
-            result = task.execute("10245").get();
+            result = task.execute("52.5200","13.4050").get();
 
         } catch (InterruptedException e) {
             e.printStackTrace();
